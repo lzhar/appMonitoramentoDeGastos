bf2b2060dbbb86f80bdda2f8596c7bf9d9fe91d6
lzhar <henrique.luiz.alves@gmail.com>
2025-08-01 21:19:34 -0300

    Primeiro commit

---
+++ src/main/java/dev/luiz/AppMonitoramentoDeGastos/AppMonitoramentoDeGastosApplication.java
@@ -0,0 +1,13 @@
+package dev.luiz.AppMonitoramentoDeGastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMonitoramentoDeGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMonitoramentoDeGastosApplication.class, args);
+	}
+
+}
