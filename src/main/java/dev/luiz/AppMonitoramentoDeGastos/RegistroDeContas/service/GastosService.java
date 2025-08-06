81f569714e9c790fc0969b8beed1a50a700eba0b
lzhar <henrique.luiz.alves@gmail.com>
2025-08-06 15:15:49 -0300

    Adiciona método para receber dados e montar o DTO De gastos

--- src/main/java/dev/luiz/AppMonitoramentoDeGastos/RegistroDeContas/service/GastosService.java
+++ src/main/java/dev/luiz/AppMonitoramentoDeGastos/RegistroDeContas/service/GastosService.java
@@ -1,6 +1,7 @@
 package dev.luiz.AppMonitoramentoDeGastos.RegistroDeContas.service;
 
 import com.opencsv.CSVWriter;
+import dev.luiz.AppMonitoramentoDeGastos.RegistroDeContas.model.*;
 import org.springframework.stereotype.Service;
 
 import java.io.FileWriter;
@@ -16,35 +17,45 @@ public class GastosService {
         return porcentagem;
     }
 
-    public void montarPlanilhaBaseadaNosDados(double gastosComPassagem, double gastosComFatura, double dinheiroDestinadoParaInvestimentos, double gastosComBeleza, double gastosComLazer){
-        double salario = 3000;
-        String caminho = "despesas.csv";
-
-
-
-        try(CSVWriter writer = new CSVWriter(new FileWriter(caminho))){
-            String passagemEmString = Double.toString(gastosComPassagem);
-            String gastosComFaturaEmString = Double.toString(gastosComFatura);
-            String dinheiroDestinadoParaInvestimentosEmString = Double.toString(dinheiroDestinadoParaInvestimentos);
-            String gastosComBelezEmString = Double.toString(gastosComBeleza);
-            String gastosComLazerEmString = Double.toString(gastosComLazer);
-            String salarioEmString = Double.toString(salario);
-
-
-
-            String[] cabecalho = {"Salario", "Gastos com passagem", "Gastos com fatura", "dinheiro para investimentos", "gastos com beleza", "gastos com lazer"};
-            writer.writeNext(cabecalho);
-
-            writer.writeNext(new String[]{salarioEmString, passagemEmString, gastosComFaturaEmString, dinheiroDestinadoParaInvestimentosEmString, gastosComBelezEmString, gastosComLazerEmString});
-
-
-            System.out.println("CSV gerado com sucesso!");
-        }catch (IOException e){
-            System.out.println(e.getMessage());
-        }
-        double porcentagemDoSalario = retornarPorcentagemNoSalario(salario, gastosComBeleza);
-        System.out.println(porcentagemDoSalario);
+    public void registrarGastoNoBanco(){
 
     }
 
+//    public void montarPlanilhaBaseadaNosDados(double gastosComPassagem, double gastosComFatura, double dinheiroDestinadoParaInvestimentos, double gastosComBeleza, double gastosComLazer){
+//        double salario = 3000;
+//        String caminho = "despesas.csv";
+//
+//
+//
+//        try(CSVWriter writer = new CSVWriter(new FileWriter(caminho))){
+//            String passagemEmString = Double.toString(gastosComPassagem);
+//            String gastosComFaturaEmString = Double.toString(gastosComFatura);
+//            String dinheiroDestinadoParaInvestimentosEmString = Double.toString(dinheiroDestinadoParaInvestimentos);
+//            String gastosComBelezEmString = Double.toString(gastosComBeleza);
+//            String gastosComLazerEmString = Double.toString(gastosComLazer);
+//            String salarioEmString = Double.toString(salario);
+//
+//
+//
+//            String[] cabecalho = {"Salario", "Gastos com passagem", "Gastos com fatura", "dinheiro para investimentos", "gastos com beleza", "gastos com lazer"};
+//            writer.writeNext(cabecalho);
+//
+//            writer.writeNext(new String[]{salarioEmString, passagemEmString, gastosComFaturaEmString, dinheiroDestinadoParaInvestimentosEmString, gastosComBelezEmString, gastosComLazerEmString});
+//
+//
+//            System.out.println("CSV gerado com sucesso!");
+//        }catch (IOException e){
+//            System.out.println(e.getMessage());
+//        }
+//        double porcentagemDoSalario = retornarPorcentagemNoSalario(salario, gastosComBeleza);
+//        System.out.println(porcentagemDoSalario);
+//
+//    }
+
+    public String receberDadosDeGastoDoCliente(GastosComLocomocao gastosComLocomocao, GastosFixos gastosComFixos,
+                          DinheiroDestinadoParaInvestimentos dinheiroDestinadoParaInvestimentos, GastosComBeleza gastosComBeleza, GastosComLazer gastosComLazer){
+        GastosDTO gastosDTO = new GastosDTO(gastosComLocomocao, gastosComFixos, dinheiroDestinadoParaInvestimentos, gastosComBeleza, gastosComLazer);
+
+        return gastosDTO.toString();
+    }
 }
